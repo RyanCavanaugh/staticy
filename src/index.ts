5f16015a24ce84294b427cc748945c57e552bd5b
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2019-02-12 14:10:19 -0800

    Remove bad dep

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import * as fileProviders from "./file-providers";
 import * as site from "./site";
-import * as typescriptRollupBundle from "./beta/typescript-rollup-bundle";
 import ServerFile from "./server-file";
 
 export * from "./text-transform";
@@ -8,6 +7,5 @@ export * from "./text-transform";
 export {
     ServerFile,
     fileProviders,
-    site,
-    typescriptRollupBundle
+    site
 };
